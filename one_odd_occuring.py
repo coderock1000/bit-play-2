4900201a743f04e5074c109655abaf0ca9614e83
coderock100 <vd.ssaathvik@gmail.com>
2024-11-16 19:53:50 +0530

    arrat odd num

--- one_odd_occuring.py
+++ one_odd_occuring.py
@@ -1,8 +1,5 @@
 def odd_occuring(arr):
-    """
-    Finds the number that occurs an odd number of times in the array.
-    Assumes that exactly one number occurs an odd number of times.
-    """
+  
     res = 0
     for element in arr:
         res ^= element  
